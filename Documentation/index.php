19ecf8397dc09d9c6344bef8448311d9b2f41e00
GEN-009 <gen-009@gen009-HP-ProBook-450-G3>
2018-11-13 10:03:09 +0100

    app php

---
+++ Documentation/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Navigateur</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+</head>
+
+<body>
+    <form class="recherche" type="text" value="faite votre recherche">
+        <input id="champ" type="text" value="rechercher"> 
+    </form>
+    
+    <script src="main.js"></script>
+</body>
+
+</html>
