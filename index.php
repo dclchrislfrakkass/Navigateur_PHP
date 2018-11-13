cc0eb5bfcbe1f1821641374c5521d0662512be9a
gen006 <gen006@gen006pc>
2018-11-13 10:07:27 +0100

    add php function for navigation

--- index.php
+++ index.php
@@ -14,6 +14,41 @@
         <input id="champ" type="text" value="rechercher"> 
     </form>
     
+<div id="navigation">
+<?php
+function list_dir($name, $level = 0)
+{
+    if ($dir = opendir($name)) {
+        while ($file = readdir($dir)) {
+            for ($i = 1; $i <= (4 * $level); ++$i) {
+                echo '&nbsp;';
+            }
+            echo "$file<br>\n";
+            if (is_dir($file) && !in_array($file, array('.', '..'))) {
+                list_dir($file, $level + 1);
+            }
+        }
+        closedir($dir);
+    }
+}
+list_dir('.');
+?>
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
     <script src="main.js"></script>
 </body>
 
